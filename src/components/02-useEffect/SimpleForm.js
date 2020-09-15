b7fd4fc4bf825f43abe3de94d4b8d987c2134bee
Jan Pierre <gamunaxx@gmail.com>
2020-09-14 22:00:13 -0500

    feat: add useEffect

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import "./effects.css";
+
+export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+  });
+
+  const { name, email } = formState;
+
+  useEffect(() => {
+    console.log('x');
+  }, []);
+
+  useEffect(() => {
+    console.log('form');
+  }, [formState]);
+
+  useEffect(() => {
+    console.log(email);
+  }, [email]);
+
+
+  const handleInputChange = ({ target }) => {
+
+    setFormState({
+      ...formState,
+      [target.name]: target.value
+    })
+  };
+
+  return (
+    <>
+      <h1>Use Effect Simple Form</h1>
+      <hr />
+
+      <div className="form-group">
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="Tu nombre"
+          autoComplete="off"
+          value={name}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <input
+          type="text"
+          name="email"
+          className="form-control"
+          placeholder="email@gmail.com"
+          autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
+        />
+      </div>
+    </>
+  );
+};
