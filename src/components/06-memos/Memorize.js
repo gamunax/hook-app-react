fc2b32e335419dfe5a0984b5e40ad7cc35967ab5
Jan Pierre <gamunaxx@gmail.com>
2021-03-05 19:13:35 -0500

    feat: add memorize

--- src/components/06-memos/Memorize.js
+++ src/components/06-memos/Memorize.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../02-useEffect/effects.css";
+
+import { useCounter } from "../../hooks/useCounter";
+import { Small } from "./Small";
 
 export const Memorize = () => {
+  const { counter, increment } = useCounter(10);
+  const [show, setshow] = useState(true);
+
   return (
     <div>
-      <h1>Memorize</h1>
+      <h1>
+        Counter: <Small value={counter} />
+      </h1>
+      <hr />
+      <button className="btn btn-primary" onClick={increment}>
+        +1
+      </button>
+
+      <button
+        className="btn btn-outline-primary ml-3"
+        onClick={() => {
+          setshow(!show);
+        }}
+      >
+        Show/Hide {JSON.stringify(show)}
+      </button>
     </div>
-  )
-}
+  );
+};
